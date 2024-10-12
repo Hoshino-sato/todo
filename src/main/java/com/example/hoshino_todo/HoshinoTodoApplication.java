1a25bdaddb8d16e6936b302720a6f43c3d06dddc
Hoshino-sato <satoshi-hoshino@los-ij.co.jp>
2024-10-12 21:35:51 +0900

    first commit

---
+++ src/main/java/com/example/hoshino_todo/HoshinoTodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.hoshino_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoshinoTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoshinoTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hoshino_todo/HoshinoTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hoshino_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoshinoTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
