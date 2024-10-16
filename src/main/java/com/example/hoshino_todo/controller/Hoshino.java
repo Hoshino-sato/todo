c6b4d6e0fefb183e0ffa1d91dfa506cdf6f1d487
Hoshino-sato <satoshi-hoshino@los-ij.co.jp>
2024-10-16 22:15:11 +0900

    出力元のクラスをtodogetQueryに変更

--- src/main/java/com/example/hoshino_todo/controller/Hoshino.java
+++ src/main/java/com/example/hoshino_todo/controller/Hoshino.java
@@ -2,12 +2,31 @@ package com.example.hoshino_todo.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import com.example.hoshino_todo.dto.TodoGetQuery;
+
+
 
 @RestController
 public class Hoshino{
-    @GetMapping("path")
 
-    public String getMethodName() {
-        return "hoge";
+    @GetMapping("path")
+    public String getMethodName(@ModelAttribute TodoGetQuery param) {
+
+        System.out.println(param.getHoshinoId());
+        System.out.println(param.getOffset());
+
+        return "hoga";
     }
-}
+
+    @PostMapping("path")
+    public String postMethodName(@RequestBody String entity) {
+
+        //TODO: process POST request
+        
+        return entity;
+    }
+
+} 
